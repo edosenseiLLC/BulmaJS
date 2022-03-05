0c03003c557ae14ab20e0061f2ca7401aa6b9bdd
Edward Johnson <github@edosensei.com>
2022-03-05 14:44:05 +0900

    edo: drupal status messages are message.js, not alert.js

--- src/bulma.js
+++ src/bulma.js
@@ -3,10 +3,11 @@
 import Bulma from './core';
 // import { Notification } from './plugins/notification';
 import { Navbar } from './plugins/navbar';
-// import { Message } from './plugins/message';
+// "Message" is actually for drupal status messages/alerts.
+import { Message } from './plugins/message';
 // import { Dropdown } from './plugins/dropdown';
 // import { Modal } from './plugins/modal';
-import { Alert } from './plugins/alert';
+// import { Alert } from './plugins/alert';
 // import { File } from './plugins/file';
 // import { Tabs } from './plugins/tabs';
 // import { PanelTabs } from './plugins/panelTabs';
